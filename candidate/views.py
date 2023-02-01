572b1a285194cce501879990772911e68656f042
nixen2802 <n.mandliya@somaiya.edu>
2023-02-01 21:11:39 +0530

    remaining work in candidate module

--- candidate/views.py
+++ candidate/views.py
@@ -17,17 +17,30 @@ from datetime import date, timedelta, datetime
 def dashboard(request, pk):
     context = JobSeeker.objects.get(user_id=pk)
     applics = Application.objects.filter(user_id=pk)
-    profile = list(ProfileVisits.objects.filter(user_id=pk))
-    fordates = Application.objects.filter(user_id=pk).order_by('-date_applied')
-    userobj=JobSeeker.objects.get(user_id=pk)
-    num_mess=Threads.objects.filter(receiver=userobj.log_id, has_unread=True)
-    num_notif=Notifications.objects.filter(rece_id=context.log_id).order_by('-datetime')
+    profile = ProfileVisits.objects.filter(user_id=pk)
+    userobj = JobSeeker.objects.get(user_id=pk)
+    num_notif = Notifications.objects.filter(rece_id=context.log_id).order_by('-datetime')
     all_notis=[]
-    threadval=Threads.objects.filter(receiver=userobj.log_id, has_unread=1)
+    threads = Threads.objects.filter(receiver=userobj.log_id)
+    threadval = threads.filter(receiver=userobj.log_id, has_unread=1)
     countunmess=0
-    for i in threadval:
-        countunmess=countunmess+len(Messages.objects.filter(msg_id=i.msg_id, is_read=0))
-    print(countunmess)
+    recent_mess=[]
+    for i in threads:
+        mess=Messages.objects.filter(msg_id=i.msg_id, receiver_user=userobj.log_id.log_id).order_by("-date")
+        if(len(mess)>0):
+            countunmess=countunmess+len(mess.filter(is_read=0))
+            tempval=[]
+            tempval.append(mess[0].date)
+            tempval.append(mess[0].body)
+            compan=Employer.objects.get(log_id=mess[0].sender_user.log_id)
+            tempval.append(compan.ename)
+            tempval.append(compan.logo)
+            recent_mess.append(tempval)
+    recent_mess.sort(reverse=True)
+    recent_mess_temp=[]
+    idx=min(4, len(recent_mess))
+    for i in range(0, idx):
+        recent_mess_temp.append(recent_mess[i])
     count=0
     for i in num_notif:
         single_notis={}
@@ -50,49 +63,105 @@ def dashboard(request, pk):
         singappli['ename']=com.ename
         singappli['logo']=com.logo
         all_applics.append(singappli)
-    graph_val=[]
-    dates=[]
-    for i in range(0, 7):
-        d=date.today()-timedelta(days=i)
-        temp = applics.filter(date_applied__year=d.year, date_applied__month=d.month, date_applied__day=d.day)
-        graph_val.append(len(temp))
-        dates.append(d)
-        # appsl=Application.objects.filter(date_applied__gte=d)
-    graph_val30=[]
-    dates30=[]
-    mapin={}
-    for i in range(0, 30):
-        d=date.today()-timedelta(days=i)
-        temp = len(applics.filter(date_applied__year=d.year, date_applied__month=d.month, date_applied__day=d.day))
-        graph_val30.append(temp)
-        if temp in mapin.keys():
-            mapin[temp].append(d)
-        else:
-            mapin[temp]=[d]
-    graph_val30.sort(reverse=True)
-    final30=[]
-    datefinal30=[]
-    finaldic={}
-    for i in graph_val30:
-        if(len(final30)>=7):
-            break
-        for j in mapin[i]:
-            if(len(datefinal30)>=7):
+    def cacl2(par):
+        graph_val=[]
+        dates=[]
+        count_7=0
+        for i in range(0, 7):
+            d=date.today()-timedelta(days=i)
+            temp=None
+            if(par=="a"):
+                temp = applics.filter(date_applied__year=d.year, date_applied__month=d.month, date_applied__day=d.day)
+            else:
+                temp=profile.filter(visiting_time__year=d.year, visiting_time__month=d.month, visiting_time__day=d.day)
+            graph_val.append(len(temp))
+            dates.append(d)
+            count_7=count_7+len(temp)
+        graph_val.reverse()
+        dates.reverse()
+        return graph_val, dates, count_7
+    graph_val, dates, count_7=cacl2("a")
+    graph_val2, dates2, vcount_7=cacl2("v")
+    def cacl(dur, par):
+        graph_val=[]
+        dates=[]
+        mapin={}
+        count=0
+        for i in range(0, dur):
+            d=date.today()-timedelta(days=i)
+            temp=0
+            if par=="a":
+                temp = len(applics.filter(date_applied__year=d.year, date_applied__month=d.month, date_applied__day=d.day))
+            else:
+                temp=len(profile.filter(visiting_time__year=d.year, visiting_time__month=d.month, visiting_time__day=d.day))
+            graph_val.append(temp)
+            count=count+temp
+            if temp in mapin.keys():
+                mapin[temp].append(d)
+            else:
+                mapin[temp]=[d]
+        graph_val.sort(reverse=True)
+        final30=[]
+        datefinal30=[]
+        finaldic={}
+        for i in graph_val:
+            if(len(final30)>=7):
                 break
-            if j not in datefinal30:
-                final30.append(i)
-                datefinal30.append(j)
-                finaldic[j]=i
-    datefinal30.sort()
-    pas30=[]
-    pasdat30=[]
-    for i in datefinal30:
-        pas30.append(finaldic[i])
-        pasdat30.append(i)
+            for j in mapin[i]:
+                if(len(datefinal30)>=7):
+                    break
+                if j not in datefinal30:
+                    final30.append(i)
+                    datefinal30.append(j)
+                    finaldic[j]=i
+        datefinal30.sort()
+        pas30=[]
+        pasdat30=[]
+        for i in datefinal30:
+            pas30.append(finaldic[i])
+            pasdat30.append(i)
+        return pas30, pasdat30, count
+    pas30, pasdat30, count_30 = cacl(30, "a")
+    pas60, pasdat60, count_60 = cacl(60, "a")
+    pas90, pasdat90, count_90 = cacl(90, "a")
+    pas365, pasdat365, count_365 = cacl(365, "a")
+    vis30, visdat30, vcount_30 = cacl(30, "v")
+    vis60, visdat60, vcount_60 = cacl(60, "v")
+    vis90, visdat90, vcount_90 = cacl(90, "v")
+    vis365, visdat365, vcount_365 = cacl(365, "v")
     charts_context={}
+    charts_context['pastsev']=dumps(graph_val)
+    charts_context['dates']=dumps(dates, default=str)
+    charts_context['count_7']=dumps(count_7)
     charts_context['pastthi']=dumps(pas30)
     charts_context['dates30']=dumps(pasdat30, default=str)
-    return render(request, 'dashboard-candidate.html', {'user': context, 'applications': all_applics, 'pk': pk, 'profile': profile, 'count': len(num_mess), 'notifics': all_notis, 'messcount': countunmess, 'pastsev': dumps(graph_val), 'dates': dumps(dates, default=str), 'charts': charts_context})
+    charts_context['count_30']=dumps(count_30)
+    charts_context['pastsix']=dumps(pas60)
+    charts_context['dates60']=dumps(pasdat60, default=str)
+    charts_context['count_60']=dumps(count_60)
+    charts_context['pastnin']=dumps(pas90)
+    charts_context['dates90']=dumps(pasdat90, default=str)
+    charts_context['count_90']=dumps(count_90)
+    charts_context['pastyea']=dumps(pas365)
+    charts_context['dates365']=dumps(pasdat365, default=str)
+    charts_context['count_365']=dumps(count_365)
+
+    charts_context['vpastsev']=dumps(graph_val2)
+    charts_context['vdates']=dumps(dates2, default=str)
+    charts_context['vcount_7']=dumps(vcount_7)
+    charts_context['vpastthi']=dumps(vis30)
+    charts_context['vdates30']=dumps(visdat30, default=str)
+    charts_context['vcount_30']=dumps(vcount_30)
+    charts_context['vpastsix']=dumps(vis60)
+    charts_context['vdates60']=dumps(visdat60, default=str)
+    charts_context['vcount_60']=dumps(vcount_60)
+    charts_context['vpastnin']=dumps(vis90)
+    charts_context['vdates90']=dumps(visdat90, default=str)
+    charts_context['vcount_90']=dumps(vcount_90)
+    charts_context['vpastyea']=dumps(vis365)
+    charts_context['vdates365']=dumps(visdat365, default=str)
+    charts_context['vcount_365']=dumps(vcount_365)
+    return render(request, 'dashboard-candidate.html', {'user': context, 'applications': all_applics, 'pk': pk, 'profile': profile, 'notifics': all_notis, 'messcount': countunmess, 'charts': charts_context, 'recent': recent_mess_temp})
 
 
 def jobapp(request, pk):
@@ -261,6 +330,7 @@ def inbox(request, pk):
     empls=[]
     temp_empls=[]
     first=None
+    recent_mess=[]
     for i in threads:
         mess=Messages.objects.filter(msg_id=i.msg_id)
         messages.append(dumps(list(mess.values()), default=str))
@@ -269,9 +339,16 @@ def inbox(request, pk):
         logs_info2=Employer.objects.filter(log_id=i.sender.log_id)
         temp_empls.append(logs_info)
         empls.append(dumps(list(logs_info2.values()), default=str))
+        recent=mess.order_by('-date')
+        c_u=len(mess.filter(is_read=0))
+        if(len(recent)>0):
+            if(str(recent[0].sender_user)==str(user.log_id)):
+                recent_mess.append(["c", recent[0].body, str(recent[0].date.hour)+":"+str(recent[0].date.minute), c_u])
+            else:
+                recent_mess.append(["e", recent[0].body, str(recent[0].date.hour)+":"+str(recent[0].date.minute), c_u])
     if(temp_empls):
         first=temp_empls[0]
-    return render(request, 'inbox-candidate.html', {'pk': pk, 'threads': threads, 'first': first ,'mess': messages, 'thre': temp_threads, 'm': temp_messages, 'emp': empls, 'initial': zip(threads, temp_empls)})
+    return render(request, 'inbox-candidate.html', {'pk': pk, 'threads': threads, 'first': first ,'mess': messages, 'thre': temp_threads, 'm': temp_messages, 'emp': empls, 'initial': zip(threads, temp_empls, recent_mess)})
 
 
 def under_development(request, pk):
@@ -343,9 +420,12 @@ def fetchmess(request, pk):
     mess_all=[]
     urlval=""
     count=0
+    ind_unread=[]
     for i in threads:
         mess=Messages.objects.filter(msg_id=i.msg_id, is_read=False)
-        count=len(mess)+count
+        si=len(mess)
+        count=si+count
+        ind_unread.append([i.msg_id, si])
         messages.append(dumps(list(mess.values()), default=str))
         temp_messages.append(mess)
         mess=Messages.objects.filter(msg_id=i.msg_id)
@@ -356,7 +436,7 @@ def fetchmess(request, pk):
     messa=dumps(list(Messages.objects.filter(msg_id=request.GET['employer'], is_read=False).values()), default=str)
     company=dumps(list(Employer.objects.filter(log_id=comp_log).values()), default=str)
     urlval=Employer.objects.filter(log_id=comp_log)[0].logo.url
-    return JsonResponse({'message': 'Y', 'url': "", 'mess': messages, 'thre': temp_threads, 'count': count, 'thread': thread, 'company': company ,'messa': messa, 'all_mess': mess_all, 'image': urlval})
+    return JsonResponse({'message': 'Y', 'url': "", 'mess': messages, 'thre': temp_threads, 'count': count, 'thread': thread, 'company': company ,'messa': messa, 'all_mess': mess_all, 'image': urlval, 'unread': dumps(ind_unread)})
 
 def seenmes(request, pk):
     if(request.method=="POST"):
